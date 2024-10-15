8edfcd464f74889a946894da37e812f70f1e93a0
Jerome Maderazo <jeromemaderazo@192.168.1.27>
2024-10-15 20:23:56 +0200

    solved

--- 1/mux/soluzione.ml
+++ 1/mux/soluzione.ml
@@ -9,10 +9,10 @@ let mux2_3 (s0:bool) (a:bool) (b:bool) : bool = match s0 with
 
 let mux4 s1 s0 a0 a1 a2 a3 =
   match (s1, s0) with
-  | (false, false) -> a0  (* Seleziona a0 *)
-  | (false, true) -> a1  (* Seleziona a1 *)
-  | (true, false) -> a2  (* Seleziona a2 *)
-  | (true, true) -> a3;; (* Seleziona a3 *)
+  | (false, false) -> a0 
+  | (false, true) -> a1 
+  | (true, false) -> a2  
+  | (true, true) -> a3;;
 
 let mux4 s1 s0 a0 a1 a2 a3 = mux2_2 s1 (mux2_2 s0 a0 a1) (mux2_2 s0 a2 a3);; 
 
